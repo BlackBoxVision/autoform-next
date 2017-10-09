6e2585a5b826e458c08f66c6dd5eeb54bc9eb660
jonatansalas <jonatansalas@live.com.ar>
2017-10-08 22:09:59 -0300

    Added withFormData HOC to access FormDataProvider context data

---
+++ src/autoform/components/hoc/withFormData.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function withFormData(mapFormDataToProps) {
+    return WrappedComponent => class WithFormData extends React.Component {
+        static contextTypes = {
+            formProps: PropTypes.object.isRequired,
+            uiAdapter: PropTypes.object.isRequired
+        };
+
+        constructor(props, context) {
+            super(props, context);
+        }
+
+        render() {
+            const newProps = this.getNewProps();
+
+            return <WrappedComponent {...newProps}/>
+        }
+
+        getNewProps = () => {
+            const { formProps, uiAdapter } = this.context;
+            const newFormProps = mapFormDataToProps ? mapFormDataToProps(formProps) : formProps;
+
+            return {
+                ...WrappedComponent.props,
+                ...newFormProps,
+                uiAdapter
+            };
+        }
+    }
+}
