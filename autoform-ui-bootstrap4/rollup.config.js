5d98682614684aabe5f4df62e80b1f095ef87c8a
jonatansalas <jonatansalas@live.com.ar>
2017-10-20 00:21:49 -0300

    Start to work on Rollup config. This is not finished yet :/

--- autoform-ui-bootstrap4/rollup.config.js
+++ autoform-ui-bootstrap4/rollup.config.js
@@ -1,3 +1,35 @@
-export default {
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 
-};
+import pkg from './package.json';
+
+export default [
+    {
+		entry: 'src/index.js',
+		dest: pkg.browser,
+		format: 'umd',
+		plugins: [
+            resolve(), 
+            babel({ runtimeHelpers: true }),
+			commonjs() 
+		]
+    },
+    {
+		entry: 'src/index.js',
+		external: Object.keys(pkg.dependencies),
+		targets: [
+			{ 
+                dest: pkg.main, 
+                format: 'cjs'
+            },
+			{
+                dest: pkg.module, 
+                format: 'es' 
+            }
+        ],
+        plugins: [
+            babel({ runtimeHelpers: true })
+		]
+	}
+];
