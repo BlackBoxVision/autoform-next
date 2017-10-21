dbe65d0ae5b57244ba90dfdc20d31a0812719273
jonatansalas <jonatansalas@live.com.ar>
2017-10-21 11:31:35 -0300

    Working to resolve issues with rollup.config.js files

--- autoform-core/rollup.config.js
+++ autoform-core/rollup.config.js
@@ -1,24 +1,41 @@
+import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 import pkg from './package.json';
 
 export default [
+	// browser-friendly UMD build
     {
-        name: 'autoform-core',
+        name: 'autoForm',
         input: 'src/index.js',
         output: {
             file: pkg.browser,
             format: 'umd'
         },
         external: ['react'],
-        plugins: [babel({ runtimeHelpers: true }), commonjs({
-            exclude: [
-              'node_modules/react/**',
-              'node_modules/react-dom/**'
-            ]
-        })]
+        globals: {
+            react: 'React'
+        },
+        plugins: [
+            resolve(),
+            babel({ 
+                runtimeHelpers: true,
+                exclude: ['node_modules/**']
+            }),
+            commonjs({
+                exclude: [
+                'node_modules/react/**',
+                'node_modules/react-dom/**'
+                ]
+            })
+        ]
     },
+    // CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// the `output` option which can specify `file` and `format`)
     {
         input: 'src/index.js',
         output: [
@@ -32,11 +49,18 @@ export default [
             }
         ],
         external: ['react'],
-        plugins: [babel({ runtimeHelpers: true }), commonjs({
-            exclude: [
-              'node_modules/react/**',
-              'node_modules/react-dom/**'
-            ]
-        })]
+        plugins: [
+            resolve(),
+            babel({ 
+                runtimeHelpers: true,
+                exclude: ['node_modules/**']
+            }),
+            commonjs({
+                exclude: [
+                'node_modules/react/**',
+                'node_modules/react-dom/**'
+                ]
+            })
+        ]
     }
 ];
--- autoform-ui-bootstrap4/rollup.config.js
+++ autoform-ui-bootstrap4/rollup.config.js
@@ -1,24 +1,52 @@
+import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 import pkg from './package.json';
 
 export default [
+	// browser-friendly UMD build
     {
-        name: 'autoform-ui-bootstrap4',
+        name: 'autoFormBootstrap',
         input: 'src/index.js',
         output: {
             file: pkg.browser,
             format: 'umd',
         },
         external: ['react'],
-        plugins: [babel({ runtimeHelpers: true }), commonjs({
-            exclude: [
-              'node_modules/react/**',
-              'node_modules/react-dom/**'
-            ]
-        })]
+        globals: {
+            react: 'React',
+            css: 'classnames',
+            PropTypes: 'prop-types',
+            reactstrap: 'reactstrap',
+            _extends: 'babel-runtime/helpers/extends',
+            _inherits: 'babel-runtime/helpers/inherits',
+            _createClass: 'babel-runtime/helpers/createClass',
+            _defineProperty: 'babel-runtime/helpers/defineProperty',
+            _classCallCheck: 'babel-runtime/helpers/classCallCheck',
+            _Object$getPrototypeOf: 'babel-runtime/core-js/object/get-prototype-of',
+            _objectWithoutProperties: 'babel-runtime/helpers/objectWithoutProperties',
+            _possibleConstructorReturn: 'babel-runtime/helpers/possibleConstructorReturn',
+        },
+        plugins: [
+            resolve(),
+            babel({ 
+                runtimeHelpers: true,
+                exclude: ['node_modules/**']
+            }),
+            commonjs({
+                exclude: [
+                'node_modules/react/**',
+                'node_modules/react-dom/**'
+                ]
+            })
+        ]
     },
+    // CommonJS (for Node) and ES module (for bundlers) build.
+	// (We could have three entries in the configuration array
+	// instead of two, but it's quicker to generate multiple
+	// builds from a single configuration where possible, using
+	// the `output` option which can specify `file` and `format`)
     {
         input: 'src/index.js',
         output: [
@@ -32,11 +60,18 @@ export default [
             }
         ],
         external: ['react'],
-        plugins: [babel({ runtimeHelpers: true }), commonjs({
-            exclude: [
-              'node_modules/react/**',
-              'node_modules/react-dom/**'
-            ]
-        })]
+        plugins: [
+            resolve(),
+            babel({ 
+                runtimeHelpers: true,
+                exclude: ['node_modules/**']
+            }),
+            commonjs({
+                exclude: [
+                'node_modules/react/**',
+                'node_modules/react-dom/**'
+                ]
+            })
+        ]
     }
 ];
