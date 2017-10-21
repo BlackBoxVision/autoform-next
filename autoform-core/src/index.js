2870983682ad013ba0cfeaae7317264f7746ebef
jonatansalas <jonatansalas@live.com.ar>
2017-10-21 18:50:26 -0300

    Fix issue with autoform-core. Now rollup builds it well!

--- autoform-core/src/index.js
+++ autoform-core/src/index.js
@@ -5,11 +5,11 @@ import AutoFormSubmit from './components/form/FormSubmit';
 import AutoFormFieldArray from './components/form/FormFieldArray';
 import AutoFormFieldEntity from './components/form/FormFieldEntity';
 
-export {
-    AutoForm,
-    AutoFormField,
-    AutoFormGroup,
-    AutoFormSubmit,
-    AutoFormFieldArray,
-    AutoFormFieldEntity
+export default {
+    AutoForm: props => <AutoForm {...props} />,
+    AutoFormField: props => <AutoFormField {...props} />,
+    AutoFormGroup: props => <AutoFormGroup {...props} />,
+    AutoFormSubmit: props => <AutoFormSubmit {...props} />,
+    AutoFormFieldArray: props => <AutoFormFieldArray {...props} />,
+    AutoFormFieldEntity: props => <AutoFormFieldEntity {...props} />
 };
